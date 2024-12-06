67092590741e9c154554ce246abdd8d0faaa2c60
Alok pradhan <137865136+Alok6371@users.noreply.github.com>
2024-12-06 20:41:06 +0530

    Video 1 completed

---
+++ VIDEO-1/script.js
@@ -0,0 +1,111 @@
+// gsap.to("#box", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1
+// })
+
+// gsap.from("#box1", {
+//     rotate: 360,
+//     x: 900,
+//     duration: 3,
+//     borderRadius: "50%",
+//     backgroundColor: "purple",
+//     // scale: 2.3
+// })
+
+
+// gsap.from("h1", {
+//     opacity: 0,
+//     color: "red",
+//     duration: 2,
+//     delay: 1,
+//     y: 40,
+//     stagger: 1,
+//     // stagger: -1,
+//     duration:3
+// })
+
+
+// gsap.to("#box", {
+//     x: 500,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     borderRadius:"50%",
+//     repeat: 2,//run {given data +1} times
+//     // yoyo:true//for repret
+// })
+
+
+
+////////////////////////////////////   TIME LINE////
+// gsap.to("#box", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 1
+// })
+// gsap.to("#box1", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 2.5,
+//     backgroundColor: "red"
+// })
+// gsap.to("#box2", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 4.5,
+//     // scale:1.5,
+//     borderRadius:"50%",
+//     backgroundColor: "red"
+// })
+
+// var tl = gsap.timeline()
+// tl.to("#box", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 1
+// })
+// tl.to("#box1", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 1,
+//     borderColor:"pink"
+// })
+// tl.to("#box2", {
+//     x: 800,
+//     rotate: 360,
+//     duration: 2,
+//     delay: 1
+// })
+
+var tl = gsap.timeline()
+// tl.from("h2", {
+//     y: -20,
+//     opacity: 0,
+//     duration: 1,
+//     delay: 0.55
+// })
+// tl.from("h4", {
+//     y: -20,
+//     opacity: 0,
+//     duration: 1,
+//     delay: 0.55,
+//     stagger: 1
+// })
+
+// tl.from("#p", {
+//     // y: 20,
+//     opacity: 0,
+//     duration: 1,
+//     scale: 0.2
+// })
+// tl.to("#p",{
+//     scale:1.5,
+//     duration:1,delay:-1
+// })
+
